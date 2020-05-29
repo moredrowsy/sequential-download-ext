0ebf161521ce616fe025d89e2399b6cedce62f3d
Thuan Tang <thuantang@gmail.com>
2020-05-29 12:24:40 -0700

    Added comment as reminder that StrictMode renders component twice in dev mode

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -5,6 +5,8 @@ import './index.css';
 import Popup from './Popup';
 
 ReactDOM.render(
+  // StrictMode renders components TWICE (on development but not production)
+  // in order to detect/warn any problems with your code.
   <React.StrictMode>
     <Popup />
   </React.StrictMode>,
